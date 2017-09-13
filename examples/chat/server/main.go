07206e36ffdb792679eac43849385fdf7dad6a4d
Zealous-w <zealous7@163.com>
2017-09-13 11:40:07 +0800

    Update main.go

--- examples/chat/server/main.go
+++ examples/chat/server/main.go
@@ -13,7 +13,7 @@ func main() {
 	queue := cellnet.NewEventQueue()
 
 	peer := socket.NewAcceptor(queue).Start("127.0.0.1:8801")
-	peer.SetName("client")
+	peer.SetName("server")
 
 	cellnet.RegisterMessage(peer, "chatproto.ChatREQ", func(ev *cellnet.Event) {
 		msg := ev.Msg.(*chatproto.ChatREQ)
